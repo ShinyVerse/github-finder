f2e972cf8d0f0fd375afa543a5df0829b617efa7
Laura Jackson <playeronereadystart@gmail.com>
2020-03-10 13:17:18 +0000

    add clear functionality for search, refactor app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Navbar from './components/layout/Navbar/Navbar';
 import Users from './components/layout/Users/Users';
+import Search from './components/layout/Search/Search';
 
 import './App.css';
 
@@ -11,25 +12,42 @@ class App extends Component {
     loading: false
   }
 
-  async componentDidMount () {
+  //search github users
+  searchUsers = async (text) => {
     this.setState({ loading: true });
 
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+ 
     this.setState({
-      users: res.data,
+      users: res.data.items,
       loading: false
     })
   }
 
+  clearUsers = () => {
+    this.setState({
+      users: [],
+      loading: false
+    })
+  }
+
+  shouldShowClearBtn = () => {
+    return this.state.users.length > 0
+  }
+
   render() {
+    const { users, loading} = this.state;
     return (
       <div className="App">
         <Navbar  />
         <div style={{border: 'black solid 1px'}} className="container">
+          <Search 
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.shouldShowClearBtn()} />
           <Users 
-          loading={this.state.loading}
-          users={this.state.users} />
+          loading={loading}
+          users={users} />
         </div>
 
       </div>
